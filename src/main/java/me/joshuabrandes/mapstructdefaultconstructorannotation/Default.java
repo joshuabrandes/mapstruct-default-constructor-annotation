acd226d97e868e6a9acbd4aec6aa9905b6839398
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2022-04-05 11:38:09 +0200

    Reticulating splines...

---
+++ src/main/java/me/joshuabrandes/mapstructdefaultconstructorannotation/Default.java
@@ -0,0 +1,9 @@
+package me.joshuabrandes.mapstructdefaultconstructorannotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.CONSTRUCTOR)
+@Documented
+public @interface Default {
+}
